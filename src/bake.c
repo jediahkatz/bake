ebbc21d018526286bce12d1e08948de5874564ef
Jediah Katz <jediahkatz@gmail.com>
2020-05-18 21:56:42 -0400

    Implement recursive baking

--- src/bake.c
+++ src/bake.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <search.h>
 #include <string.h>
+#include <unistd.h>
 #include "parser.h"
 
 void main(int argc, char **argv) {
-    // Read bakefile into string buffer
-    FILE *f = fopen("bakefile.txt", "rb");
+    // Read Bakefile into string buffer
+    FILE *f = fopen("Bakefile", "rb");
     if (f == NULL) {
-        perror("Can't open bakefile:");
+        perror("Can't open Bakefile:");
     }
     // Get length of file and count number of ':'
     int c, fsize = 0, numrules = 0;
@@ -34,17 +35,40 @@ void main(int argc, char **argv) {
 
     // Parse bakefile into rules and map targets to rule
     RULE_PARSER *rparser = init_rule_parser(bakefile);
-    RULE *rule, *rule_to_bake = NULL;
+    RULE *rule;
     hcreate(numrules);
     while ((rule = get_next_rule(rparser)) != NULL) {
-        // Save the target rule specified in CLI args (or the first rule if none specified)
-        if (rule_to_bake == NULL && (bake_target == NULL || !strcmp(rule->target, bake_target))) {
-            rule_to_bake = rule;
+        // Target the first rule in the Bakefile if none was specified in CLI args
+        if (bake_target == NULL) {
+            bake_target = rule->target;
         }
         ENTRY entry = { .key = rule->target, .data = rule };
         // TODO: check for duplicates?
         hsearch(entry, ENTER);
     }
 
-    
+    bake(bake_target);
+}
+
+/** Recursively bake all dependencies. Then, if this target was out-of-date
+ *  (i.e., it does not exist or any of its dependencies or descendants are newer),
+ *  run the recipe for rule associated with the current target.
+ *  Returns 1 if this target was out-of-date.
+ */
+int bake(char *target) {
+    ENTRY entry = { .key = target };
+    RULE *rule = hsearch(entry, FIND);
+    int out_of_date = 0;
+    if (rule != NULL) {
+        PREREQ *prereq;
+        SLIST_FOREACH(prereq, rule->prereqs_list, entries) {
+            out_of_date |= bake(prereq->filename);
+        }
+    }
+    // Without a rule, the target file is only out-of-date if it doesn't exist
+    out_of_date |= (access(target, F_OK ) == -1);
+    if (out_of_date && rule != NULL) {
+        system(rule->recipe);
+    }
+    return out_of_date;
 }
